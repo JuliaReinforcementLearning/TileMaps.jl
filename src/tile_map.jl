b03b80dedbc9eda3cc4a564d66fd3b4b695b8cbb
Siddharth Bhatia <sidb60200@gmail.com>
2021-05-24 00:18:39 +0530

    add to_index, getindex, setindex! for TileMap

--- src/tile_map.jl
+++ src/tile_map.jl
@@ -20,3 +20,12 @@ Base.setindex!(tile_map::TileMap, args...; kwargs...) = Base.setindex!(tile_map.
 get_num_objects(tile_map::TileMap) = size(grid, 1)
 get_height(tile_map::TileMap) = size(grid, 2)
 get_width(tile_map::TileMap) = size(grid, 3)
+
+@generated function Base.to_index(tile_map::TileMap{O}, object::X) where {X <: AbstractObject, O}
+    i = findfirst(X .=== O.parameters)
+    isnothing(i) && error("Object $object is not present in $tile_map")
+    return :($i)
+end
+
+Base.getindex(tile_map::TileMap, object::AbstractObject, args...) = getindex(tile_map.grid, Base.to_index(tile_map, object), args...)
+Base.setindex!(tile_map::TileMap, value::Bool, object::AbstractObject, args...) = setindex!(tile_map.grid, value, Base.to_index(tile_map, object), args...)
