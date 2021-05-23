6c419a9a6b2c35e88c986d41441c0ee4569f50db
Sid-Bhatia-0 <32610387+Sid-Bhatia-0@users.noreply.github.com>
2021-05-23 23:55:43 +0530

    add objects, directions, and TileMap struct (#1)
    * add directions.jl
    
    * add objects.jl
    
    * add tile_map.jl
    
    * include directions.jl, objects.jl, tile_map.jl in TileMaps.jl

---
+++ src/objects.jl
@@ -0,0 +1,10 @@
+abstract type AbstractObject end
+
+struct Object1 <: AbstractObject end
+const OBJECT_1 = Object1()
+
+struct Object2 <: AbstractObject end
+const OBJECT_2 = Object2()
+
+struct Object3 <: AbstractObject end
+const OBJECT_3 = Object3()
---
+++ src/tile_map.jl
@@ -0,0 +1,18 @@
+"""
+    TileMap{O} <: AbstractArray{Bool, 3}
+The first dimension uses multi-hot encoding to encode objects in a tile.
+The second and third dimensions correspond to the height and width of the tile map respectively.
+"""
+struct TileMap{O} <: AbstractArray{Bool, 3}
+    grid::BitArray{3}
+    objects::O
+end
+
+function TileMap(objects::Tuple{Vararg{AbstractObject}}, height::Integer, width::Integer)
+    grid = falses(length(objects), height, width)
+    return TileMap(grid, objects)
+end
+
+Base.size(tile_map::TileMap, args...; kwargs...) = Base.size(tile_map.grid, args..., kwargs...)
+Base.getindex(tile_map::TileMap, args...; kwargs...) = Base.getindex(tile_map.grid, args..., kwargs...)
+Base.setindex!(tile_map::TileMap, args...; kwargs...) = Base.setindex!(tile_map.grid, args..., kwargs...)
