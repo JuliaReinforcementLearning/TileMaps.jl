5483b2b5c2620ec6d778573c9cf7a3ab68ca29f0
Siddharth Bhatia <sidb60200@gmail.com>
2021-05-27 01:39:00 +0530

    add indexing using array of objects

--- src/tile_map.jl
+++ src/tile_map.jl
@@ -14,18 +14,24 @@ function TileMap(objects::Tuple{Vararg{AbstractObject}}, height::Integer, width:
 end
 
 Base.size(tile_map::TileMap, args...; kwargs...) = Base.size(tile_map.grid, args..., kwargs...)
-Base.getindex(tile_map::TileMap, args...; kwargs...) = Base.getindex(tile_map.grid, args..., kwargs...)
-Base.setindex!(tile_map::TileMap, args...; kwargs...) = Base.setindex!(tile_map.grid, args..., kwargs...)
-
 get_num_objects(tile_map::TileMap) = size(tile_map, 1)
 get_height(tile_map::TileMap) = size(tile_map, 2)
 get_width(tile_map::TileMap) = size(tile_map, 3)
 
+# regular indexing (indexing without using objects)
+Base.getindex(tile_map::TileMap, args...; kwargs...) = Base.getindex(tile_map.grid, args..., kwargs...)
+Base.setindex!(tile_map::TileMap, args...; kwargs...) = Base.setindex!(tile_map.grid, args..., kwargs...)
+
+# indexing using one object
 @generated function Base.to_index(tile_map::TileMap{O}, object::X) where {X <: AbstractObject, O}
     i = findfirst(X .=== O.parameters)
     isnothing(i) && error("Object $object is not present in $tile_map")
     return :($i)
 end
 
-Base.getindex(tile_map::TileMap, object::AbstractObject, args...) = getindex(tile_map.grid, Base.to_index(tile_map, object), args...)
-Base.setindex!(tile_map::TileMap, value::Bool, object::AbstractObject, args...) = setindex!(tile_map.grid, value, Base.to_index(tile_map, object), args...)
+Base.getindex(tile_map::TileMap, object::AbstractObject, args...; kwargs...) = getindex(tile_map.grid, Base.to_index(tile_map, object), args..., kwargs...)
+Base.setindex!(tile_map::TileMap, value::Bool, object::AbstractObject, args...; kwargs...) = setindex!(tile_map.grid, value, Base.to_index(tile_map, object), args..., kwargs...)
+
+# indexing using more than one object
+Base.to_index(tile_map::TileMap, objects::AbstractArray{<:AbstractObject}) = map(object -> Base.to_index(tile_map, object), objects)
+Base.getindex(tile_map::TileMap, objects::AbstractArray{<:AbstractObject}, args...; kwargs...) = getindex(tile_map.grid, map(object -> Base.to_index(tile_map, object), objects), args..., kwargs...)
