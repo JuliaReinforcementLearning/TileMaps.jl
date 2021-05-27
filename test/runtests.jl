00a10be9fc2cbab748ca8589a059fa95fbe6c213
Sid-Bhatia-0 <32610387+Sid-Bhatia-0@users.noreply.github.com>
2021-05-27 15:00:06 +0530

    add indexing using array of objects (#7)
    * add indexing using array of objects
    
    * add tests for indexing using array of objects

--- test/runtests.jl
+++ test/runtests.jl
@@ -27,11 +27,20 @@ Test.@testset "TileMaps.jl" begin
 
     tile_map = TM.TileMap(grid, objects)
 
+    # regular indexing (indexing without using objects)
+    Test.@test tile_map[1, 2, 3] == false
+    Test.@test tile_map[1:3, 2, 2] == BitArray([1, 0, 1])
+    Test.@test tile_map[[1, 3], 2, 2] == BitArray([1, 1])
+
+    # indexing using one object
     Test.@test tile_map[TM.EXAMPLE_OBJECT_2, 1, 1] == tile_map[2, 1, 1] == false
     Test.@test tile_map[TM.EXAMPLE_OBJECT_2, end, end] == tile_map[2, end, end] == false
     Test.@test tile_map[TM.EXAMPLE_OBJECT_2, :, 1] == tile_map[2, :, 1] == BitArray([0, 0, 1, 1])
     Test.@test tile_map[TM.EXAMPLE_OBJECT_2, end, :] == tile_map[2, end, :] == BitArray([1, 1, 0, 0, 0])
     Test.@test tile_map[TM.EXAMPLE_OBJECT_2, 2:3, 2:4] == tile_map[2, 2:3, 2:4] == BitArray([0 0 0
                                                                                              1 0 0])
-    Test.@test tile_map[begin:end, 2, 2] == BitArray([1, 0, 1])
+
+    # indexing using more than one object
+    Test.@test tile_map[[TM.EXAMPLE_OBJECT_2], 1, 1] == tile_map[[2], 1, 1] == BitArray([0])
+    Test.@test tile_map[[TM.EXAMPLE_OBJECT_3, TM.EXAMPLE_OBJECT_1], 2, 1] == tile_map[[3, 1], 2, 1] == BitArray([1, 0])
 end
