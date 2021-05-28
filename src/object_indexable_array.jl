77c2ab11614eb7f893119708829d57d02f620a96
Siddharth Bhatia <sidb60200@gmail.com>
2021-05-28 17:48:39 +0530

    update docstrings and comments

--- src/object_indexable_array.jl
+++ src/object_indexable_array.jl
@@ -1,3 +1,10 @@
+"""
+    ObjectIndexableArray{T, N, A, O} <: AbstractArray{T, N}
+
+An instance of `ObjectIndexableArray`, referred to as `object_indexable_array` here, simply wraps an `array` and allows us to index its first dimension using a [singleton](https://docs.julialang.org/en/v1/manual/types/#man-singleton-types) object or an array of singleton objects (in addition to all the other ways of indexing `array`). Information about the objects is stored as the type of tuple of objects in the type parameter `O` above.
+
+`size(object_indexable_array, 1)` should be equal to the number of elements in the type parameter `O`.
+"""
 struct ObjectIndexableArray{T, N, A, O} <: AbstractArray{T, N}
     array::A
 end
@@ -11,7 +18,7 @@ Base.size(object_indexable_array::ObjectIndexableArray, args...; kwargs...) = Ba
 Base.getindex(object_indexable_array::ObjectIndexableArray, args...; kwargs...) = Base.getindex(object_indexable_array.array, args..., kwargs...)
 Base.setindex!(object_indexable_array::ObjectIndexableArray, args...; kwargs...) = Base.setindex!(object_indexable_array.array, args..., kwargs...)
 
-# indexing using a single object as index
+# indexing using a single object
 @generated function Base.to_index(object_indexable_array::ObjectIndexableArray{T, N, A, O}, object::X) where {T, N, A, O, X <: AbstractObject}
     i = findfirst(X .=== O.parameters)
     isnothing(i) && error("Object $object is not present in $object_indexable_array")
@@ -21,6 +28,6 @@ end
 Base.getindex(object_indexable_array::ObjectIndexableArray, object::AbstractObject, args...; kwargs...) = getindex(object_indexable_array.array, Base.to_index(object_indexable_array, object), args..., kwargs...)
 Base.setindex!(object_indexable_array::ObjectIndexableArray, value::Bool, object::AbstractObject, args...; kwargs...) = setindex!(object_indexable_array.array, value, Base.to_index(object_indexable_array, object), args..., kwargs...)
 
-# indexing using more than one object
+# indexing using an array of objects
 Base.to_index(object_indexable_array::ObjectIndexableArray, objects::AbstractArray{<:AbstractObject}) = map(object -> Base.to_index(object_indexable_array, object), objects)
 Base.getindex(object_indexable_array::ObjectIndexableArray, objects::AbstractArray{<:AbstractObject}, args...; kwargs...) = getindex(object_indexable_array.array, map(object -> Base.to_index(object_indexable_array, object), objects), args..., kwargs...)
